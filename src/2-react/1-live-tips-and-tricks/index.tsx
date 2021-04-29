e449b01dca1b5beeb9f5c839c1b9c51e4a3b6d7d
Roman Pinyazhin <pinyazhin@haulmont.com>
2021-04-29 22:02:31 +0400

    setup tips starting point

---
+++ src/2-react/1-live-tips-and-tricks/index.tsx
@@ -0,0 +1,13 @@
+/* Core */
+import { render } from 'react-dom';
+
+/* Components */
+
+/* Instruments */
+import './theme/main.scss';
+
+render(<h1>Типсы и Триксы</h1>, document.getElementById('root'));
+
+const obj = {
+    name: 'John',
+};
